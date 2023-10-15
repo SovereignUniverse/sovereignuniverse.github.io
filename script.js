d1ccd8a5e88126c2b981105c0e8843922a09f391
SovereignUniverse <mddyatlik0307@gmail.com>
2023-10-15 06:49:33 -0700

    init

--- script.js
+++ script.js
@@ -1,8 +1,48 @@
-let tele_obj = window.Telegram.WebApp;
+var webapp = window.Telegram.WebApp;
+webapp.expand();
+var brandsTag = document.getElementById('brands');
+var modelsTag = document.getElementById('models');
+var yearsTag = document.getElementById('years');
+var distanceTag = document.getElementById('distance');
+var zipCodeTag = document.getElementById('zipCode');
+var searchTag = document.getElementById('search');
 
-tele_obj.expand();
-
-function load_brands() {
-    let brands_select = document.getElementById('brands')
-    
+function get_json(url) {
+    fetch(url).then(function(response) {
+        if (response === 200) {
+            return response.json();
+        } else {
+            // window.location.reload()
+        }
+    }).catch(function(error) {
+        // window.location.reload()
+    });
 }
+
+function fillSelectTag(selectTag, response) {
+    response.forEach(element => {
+        var option = document.createElement('option');
+        option.value = element.id;
+        option.text = element.name;
+        selectTag.appendChild(option);
+    });
+}
+
+fillSelectTag(brandsTag, get_json('https://www.picknpull.com/api/vehicle/makes'));
+
+
+brandsTag.addEventListener('change', function() {
+    var selectedOption = brandsTag.options[brandsTag.selectedIndex];
+    var response = get_json('https://www.picknpull.com/api/vehicle/makes/${selectedOption.value}/models');
+    fillSelectTag(modelsTag, response);
+});
+
+searchTag.addEventListener('click', function() {
+    data = {'brand_id': brandsTag.options[brandsTag.selectedIndex].value, 
+    'model_id': modelsTag.options[modelsTag.selectedIndex].value, 
+    'years': yearsTag.value,
+    'distance': distanceTag.value,
+    'zip_code': zipCodeTag.value}
+    webapp.sendData(data)
+    webapp.close();
+});
