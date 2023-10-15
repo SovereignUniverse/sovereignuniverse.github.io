c1dd1258dd65045652797b868186458e11b330a2
SovereignUniverse <mddyatlik0307@gmail.com>
2023-10-15 07:21:54 -0700

    init

--- script.js
+++ script.js
@@ -8,18 +8,27 @@ var zipCodeTag = document.getElementById('zipCode');
 var searchTag = document.getElementById('search');
 
 function get_json(url) {
-    fetch('https://cors-anywhere.herokuapp.com/' + url).then(function(response) {
-        if (response === 200) {
-            return response.json();
-        } else {
-            // window.location.reload()
-        }
-    }).catch(function(error) {
-        // window.location.reload()
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest(); 
+        xhr.open('GET', 'https://cors-anywhere.herokuapp.com/' + url, true);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                resolve(JSON.parse(xhr.responseText));
+            } else {
+                reject('ERROR');
+            }
+        };
+        xhr.onerror = function() {
+            reject('ERROR');
+        };
+        xhr.send();
     });
 }
 
 function fillSelectTag(selectTag, response) {
+    while (selectTag.options.length > 1)
+        selectTag.remove(1);
+    
     response.forEach(element => {
         var option = document.createElement('option');
         option.value = element.id;
@@ -28,21 +37,32 @@ function fillSelectTag(selectTag, response) {
     });
 }
 
-fillSelectTag(brandsTag, get_json('https://www.picknpull.com/api/vehicle/makes'));
+get_json('https://www.picknpull.com/api/vehicle/makes')
+    .then(function(response) {
+        fillSelectTag(brandsTag, response);
+    })
+    .catch(function(error) {
+        console.error('Ошибка:', error);
+    });
 
-
-brandsTag.addEventListener('change', function() {
+brandsTag.addEventListener('change', function () {
     var selectedOption = brandsTag.options[brandsTag.selectedIndex];
-    var response = get_json('https://www.picknpull.com/api/vehicle/makes/${selectedOption.value}/models');
-    fillSelectTag(modelsTag, response);
+    get_json('https://www.picknpull.com/api/vehicle/makes/' + selectedOption.value + '/models')
+        .then(function(response) {
+            fillSelectTag(modelsTag, response);
+        })
+        .catch(function(error) {
+            console.error('Ошибка:', error);
+        });
 });
 
-searchTag.addEventListener('click', function() {
-    data = {'brand_id': brandsTag.options[brandsTag.selectedIndex].value, 
-    'model_id': modelsTag.options[modelsTag.selectedIndex].value, 
-    'years': yearsTag.value,
-    'distance': distanceTag.value,
-    'zip_code': zipCodeTag.value}
-    webapp.sendData(data)
+searchTag.addEventListener('click', function () {
+    data = {
+        'brand_id': brandsTag.options[brandsTag.selectedIndex].value,
+        'model_id': modelsTag.options[modelsTag.selectedIndex].value,
+        'years': yearsTag.value,
+        'distance': distanceTag.value,
+        'zip_code': zipCodeTag.value};
+    webapp.sendData(data);
     webapp.close();
 });
